0a2afb7fa0a1cd9477597a477c5429e9aa65fc0f
karthikck <karthikck@deloitte.com>
2022-04-27 19:55:58 +0530

    Api Testing Test Cases Added

--- src/test/java/Tests/GetCandidatesList.java
+++ src/test/java/Tests/GetCandidatesList.java
@@ -6,15 +6,14 @@ import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class GetAllRecruitmentsTest {
+public class GetCandidatesList {
 
     @Test
-    public void getRecruitments(){
-        String end = "/resume/getRecruitment";
+    public void candidatesList(){
+        String end = "/resume/candidates?rec_id=18";
         Response response = getScore.login(end);
         Assert.assertEquals(response.statusCode(),200);
         JsonPath jspath = response.jsonPath();
-        //jspath.prettyPrint();
+        jspath.prettyPrint();
     }
-
 }
