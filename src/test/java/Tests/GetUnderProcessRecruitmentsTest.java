8f1a4f946638ba0f7f1aac7c4d1c4d6dd35fb621
Manoj <javvkumar@deloitte.com>
2022-04-28 10:56:35 +0530

    Added Quiz Part

---
+++ src/test/java/Tests/GetUnderProcessRecruitmentsTest.java
@@ -0,0 +1,38 @@
+package Tests;
+
+import Requests.getScore;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GetUnderProcessRecruitmentsTest {
+    @Test(priority = 1)
+    public void checkStatus(){
+        RestAssured.useRelaxedHTTPSValidation();
+        String endpoint = "resume/getUnderProcessRecruitments";
+        String id="hr_id";
+        int param=1;
+        Response response = getScore.getParam(endpoint,id,param);
+        Assert.assertEquals(response.getStatusCode(),200);
+        JSONArray arr = new JSONArray(response.asString());
+        JSONObject ele = arr.getJSONObject(1).getJSONObject("expert");
+        Assert.assertEquals(ele.get("id"),5);
+        Assert.assertEquals(ele.get("name"),"Vidushi Singh");
+        Assert.assertEquals(ele.get("username"),"vidushi");
+        Assert.assertEquals(ele.get("email"),"vidu.s1999@gmail.com");
+        Assert.assertEquals(ele.get("role"),"Subject Expert");
+    }
+    @Test(priority = 2)
+    public void checkNull(){
+        String endpoint = "resume/getUnderProcessRecruitments";
+        String id="hr_id";
+        int param=500;
+        Response response = getScore.getParam(endpoint,id,param);
+        String str=response.asString();
+        Assert.assertEquals(response.getStatusCode(),200);
+        Assert.assertEquals(str,"[]");
+    }
+}
